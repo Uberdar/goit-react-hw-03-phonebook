7186b7ef67740dea8ba44f309b937de8214c4e77
Alexandr Sanatar <allexvik@gmail.com>
2022-04-18 18:37:02 +0300

    v1

---
+++ src/components/App.jsx
@@ -0,0 +1,68 @@
+import { Component } from 'react';
+import PhonebookClass from './Phonebook/Phonebook';
+import TodosRenderClass from './TodosRenderClass/TodosRenderClass';
+import { nanoid } from 'nanoid';
+import SearchContact from './SearchContact/SearchContact';
+
+class App extends Component {
+  state = {
+    dataBase: [],
+    searchValueApp: '',
+  };
+
+  deleteWithId = DBid => {
+    // console.log('Получаю ИД контакта по кнопке удалить: ', DBid);
+    let x = this.state.dataBase;
+    // console.log('x: ', x);
+    let xx = x.filter(elem => elem.id !== DBid);
+    this.setState({
+      dataBase: xx,
+    });
+  };
+
+  formSubmitHandler = data => {
+    // console.log('споймал данные из phonebook.js: ', data);
+    const newContact = { ...data, id: nanoid() };
+    // console.log('добавляю ID ', newContact);
+    // console.log('беру старый массив:', this.state.dataBase);
+    this.setState(prevState => {
+      return {
+        dataBase: [newContact, ...prevState.dataBase],
+      };
+    });
+    // setTimeout(() => {
+    //   // console.log('показываю новый массив:', this.state.dataBase);
+    // });
+  };
+  showSearchedValues = data => {
+    this.setState({
+      searchValueApp: data.target.value,
+    });
+  };
+  filteredContacts = () => {
+    return this.state.dataBase.filter(elem =>
+      elem.name
+        .toLocaleLowerCase()
+        .includes(this.state.searchValueApp.toLocaleLowerCase())
+    );
+  };
+  render() {
+    // console.log('searchValueApp: ', this.state.searchValueApp);
+    // console.log('показываю новый массив: 42', this.state.dataBase);
+    return (
+      <div>
+        <PhonebookClass randomvarname={this.formSubmitHandler} />
+        <SearchContact
+          searchValueParse={this.showSearchedValues}
+          parseValueToSearchContact={this.state.searchValueApp}
+        />
+        <TodosRenderClass
+          datafromdataBase={this.filteredContacts()}
+          deleteSpecific={this.deleteWithId}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
